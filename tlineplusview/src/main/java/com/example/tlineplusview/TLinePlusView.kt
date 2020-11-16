387aa228a6660eacd8d8ba0b09b893f22660a223
Anwesh43 <anweshthecool0@gmail.com>
2020-11-17 01:53:24 +0530

    adding constants and libraries

--- tlineplusview/src/main/java/com/example/tlineplusview/TLinePlusView.kt
+++ tlineplusview/src/main/java/com/example/tlineplusview/TLinePlusView.kt
@@ -1,2 +1,26 @@
 package com.example.tlineplusview
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val parts : Int = 4
+val scGap : Float = 0.02f / parts
+val strokeFactor : Int = 90
+val sizeFactor : Float = 3.9f
+val delay : Long = 20
+val colors : Array<Int> = arrayOf(
+    "#F44336",
+    "#009688",
+    "#4CAF50",
+    "#FF9800",
+    "#2196F3"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val backColor : Int = Color.parseColor("#BDBDBD")
+
