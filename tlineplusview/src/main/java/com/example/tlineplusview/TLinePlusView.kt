85ddb4460323c7500a03cdcb72e53876a0a468ab
Anwesh43 <anweshthecool0@gmail.com>
2020-11-17 02:23:41 +0530

    adding Canvas extension methods

--- tlineplusview/src/main/java/com/example/tlineplusview/TLinePlusView.kt
+++ tlineplusview/src/main/java/com/example/tlineplusview/TLinePlusView.kt
@@ -23,8 +23,41 @@ val colors : Array<Int> = arrayOf(
     Color.parseColor(it)
 }.toTypedArray()
 val backColor : Int = Color.parseColor("#BDBDBD")
+val deg : Float = 180f
+val gap : Float = 90f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawTLinePlus(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sf : Float = scale.sinify()
+    val size : Float = Math.min(w, h) / sizeFactor
+    save()
+    translate(w / 2, h / 2)
+    rotate(deg * sf.divideScale(2, parts))
+    for (j in 0..1) {
+        save()
+        scale(1f, 1f - 2 * j)
+        translate(0f, (h / 2 - paint.strokeWidth) * sf.divideScale(1, parts))
+        rotate(deg * sf.divideScale(1, parts))
+        for (k in 0..2) {
+            save()
+            rotate(90f * k)
+            drawLine(0f, 0f, -size * sf.divideScale(0, parts), 0f, paint)
+            restore()
+        }
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawTLPNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawTLinePlus(scale, w, h, paint)
+}
