d77606761b288b64a2b6901aca6836ad62eaa2ae
Anwesh43 <anweshthecool0@gmail.com>
2020-11-17 02:28:46 +0530

    adding TLinePlusView class

--- tlineplusview/src/main/java/com/example/tlineplusview/TLinePlusView.kt
+++ tlineplusview/src/main/java/com/example/tlineplusview/TLinePlusView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawTLPNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawTLinePlus(scale, w, h, paint)
 }
+
+class TLinePlusView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
